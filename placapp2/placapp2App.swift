a77f141a3497a522450fb91745816738aed07456
David <xddavidxd2001@gmail.com>
2023-07-14 10:00:24 -0500

    Initial Commit

---
+++ placapp2/placapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  placapp2App.swift
+//  placapp2
+//
+//  Created by David Rojas on 13/07/23.
+//
+
+import SwiftUI
+
+@main
+struct placapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
